2eb259689b31acb7e0517ea5b97f2391880ee97a
Stephanie <stephcshea@gmail.com>
2018-05-18 08:14:18 -0400

    first repo from Visual Code

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var processList = Process.GetProcesses();
+            var process = processList.Where(p=> p.ProcessName == "dotnet").First();
+            Console.WriteLine("Processing your feelings is " + process.PriorityClass);
+        }
+    }
+}
